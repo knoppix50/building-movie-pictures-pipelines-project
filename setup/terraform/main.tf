2fcbbabee776c179601b4902f34d11365b1803ee
knoppix50 <knoppix-devops@hotmail.com>
2026-06-24 01:31:53 +0200

    Updating repository

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -202,9 +202,9 @@ resource "aws_eks_node_group" "main" {
   instance_types  = ["t3.small"]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
+    desired_size = 3
+    max_size     = 5
+    min_size     = 3
   }
 
 
